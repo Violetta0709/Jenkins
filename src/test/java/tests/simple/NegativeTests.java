4847ee75ce96f0766d0162532b2224354812e4cd
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-10-18 19:14:54 +0300

    added simple tests

--- src/test/java/tests/simple/NegativeTests.java
+++ src/test/java/tests/simple/NegativeTests.java
@@ -1,35 +1,39 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+@Disabled
 public class NegativeTests {
+    @Disabled
     @Test
     void test1() {
         assertTrue(false);
     }
-
+    @Disabled
     @Test
     void test2() {
         assertTrue(false);
     }
-
+    @Disabled
     @Test
     void test3() {
         assertTrue(false);
     }
-
+    @Disabled
     @Test
     void test4() {
         assertTrue(false);
     }
-
+    @Disabled
     @Test
     void test5() {
         assertTrue(false);
     }
-
+    @Disabled
     @Test
     void test6() {
         assertTrue(false);

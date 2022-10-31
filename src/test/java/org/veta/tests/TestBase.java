aba7539ab26bfde00632780167d3e93ecd4a3257
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-10-31 22:03:53 +0300

    Attachments

--- src/test/java/org/veta/tests/TestBase.java
+++ src/test/java/org/veta/tests/TestBase.java
@@ -2,7 +2,9 @@ package org.veta.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -22,5 +24,10 @@ public class TestBase {
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
-
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+    }
 }

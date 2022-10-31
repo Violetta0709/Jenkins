3a4beccaf8c505ef4b01fd2ae085cb5100f7977e
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-10-31 23:03:42 +0300

    steps added

--- src/test/java/org/veta/tests/WithPageObjectsJavaFakerTest.java
+++ src/test/java/org/veta/tests/WithPageObjectsJavaFakerTest.java
@@ -8,6 +8,8 @@ import org.veta.pages.components.RegistrationFormPage;
 import org.veta.testsData.Student;
 import org.veta.testsData.Student;
 
+import static io.qameta.allure.Allure.step;
+
 public class WithPageObjectsJavaFakerTest extends TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
@@ -17,29 +19,32 @@ public class WithPageObjectsJavaFakerTest extends TestBase {
     @Severity(SeverityLevel.BLOCKER)
     @Link("https://demoqa.com")
     @Test
-        void fillFormTest() {
-        registrationFormPage.openPage()
-                .setFirstName(Student.firstName)
-                .setLastName(Student.lastName)
-                .setEmail(Student.email)
-                .setGender(Student.gender)
-                .setNumber(Student.phone)
-                .setSubject(Student.subject)
-                .setHobbies(Student.hobby)
-                .uploadPic(Student.picturePath)
-                .setAddress(Student.address, Student.state, Student.city)
-                .clickSubmit();
-
-        registrationFormPage.checkResultsTableVisible()
-                .checkResult("Student Name", Student.firstName + " " + Student.lastName)
-                .checkResult("Student Email", Student.email)
-                .checkResult("Gender", Student.gender)
-                .checkResult("Mobile", Student.phone)
-                .checkResult("Subjects", Student.subject)
-                .checkResult("Hobbies", Student.hobby)
-                .checkResult("Picture", Student.picture)
-                .checkResult("Address", Student.address)
-                .checkResult("State and City", Student.state + " " + Student.city);
+    void fillFormTest() {
+        step("Fill form", () -> {
+            registrationFormPage.openPage()
+                    .setFirstName(Student.firstName)
+                    .setLastName(Student.lastName)
+                    .setEmail(Student.email)
+                    .setGender(Student.gender)
+                    .setNumber(Student.phone)
+                    .setSubject(Student.subject)
+                    .setHobbies(Student.hobby)
+                    .uploadPic(Student.picturePath)
+                    .setAddress(Student.address, Student.state, Student.city)
+                    .clickSubmit();
+        });
+        step("Check result", () -> {
+            registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", Student.firstName + " " + Student.lastName)
+                    .checkResult("Student Email", Student.email)
+                    .checkResult("Gender", Student.gender)
+                    .checkResult("Mobile", Student.phone)
+                    .checkResult("Subjects", Student.subject)
+                    .checkResult("Hobbies", Student.hobby)
+                    .checkResult("Picture", Student.picture)
+                    .checkResult("Address", Student.address)
+                    .checkResult("State and City", Student.state + " " + Student.city);
+        });
     }
 }
 

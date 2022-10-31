8f242bac2c5050aef0f4e11ea6c2ac00c2efd462
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-10-31 21:37:35 +0300

    addListener AllureSelenide

--- src/test/java/org/veta/tests/TestBase.java
+++ src/test/java/org/veta/tests/TestBase.java
@@ -1,6 +1,8 @@
 package org.veta.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -14,6 +16,7 @@ public class TestBase {
 
     @BeforeAll
     static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
